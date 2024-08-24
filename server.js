2324b64c32c9d1f10ade67d945c9292b9f649b57
APillai03 <adityapillai786@gmail.com>
2024-08-24 12:12:29 +0530

    Certain Fixes

--- server.js
+++ server.js
@@ -9,16 +9,17 @@ const port = 3000;
 // Serve static files from the 'assets' directory
 app.use(express.static(__dirname + '/assets'));
 
+// Endpoint to download the resume
+app.get("/download", (req, res) => {
+    res.download(__dirname + "/assets/Resume.pdf");
+    
+});
+
+
 // Serve the index.html file on the root path
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
-
-// Endpoint to download the resume
-app.get("/download", (req, res) => {
-    res.download(__dirname + "/assets/Resume.pdf");
-});
-
 app.listen(port, () => {
     console.log(`${port} Active`);
 });
