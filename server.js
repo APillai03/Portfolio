ae6b92e0609f2db9c925aab57555cb1e3b17a046
APillai03 <adityapillai786@gmail.com>
2024-08-25 01:06:30 +0530

    EJS Update

--- server.js
+++ server.js
@@ -1,25 +1,50 @@
 import express from "express";
-import { dirname } from "path";
+import morgan from "morgan";
+import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
-// Serve static files from the 'assets' directory
-app.use(express.static(__dirname + '/assets'));
+// Middleware to log requests
+function logger(req, res, next) {
+    console.log("Request Method: ", req.method);
+    console.log("Request URL: ", req.url);
+    next();
+}
+
+app.use(logger);
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan("combined"));
+
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // Endpoint to download the resume
 app.get("/download", (req, res) => {
-    res.download(__dirname + "/assets/Resume.pdf");
-    
+    const filePath = path.join(__dirname, 'public', 'Resume.pdf');
+    res.download(filePath, (err) => {
+        if (err) {
+            console.error("Error downloading file:", err);
+            res.status(500).send("Error downloading file.");
+        }
+    });
 });
 
-
-// Serve the index.html file on the root path
+// Serve the index.ejs file on the root path
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+    res.render("index"); 
 });
+
+app.get("/contact", (req, res) => {
+    res.render("contact"); 
+});
+
+
 app.listen(port, () => {
-    console.log(`${port} Active`);
+    console.log(`Server running on port ${port}`);
 });
